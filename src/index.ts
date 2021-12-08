640c79bddd0f20451141e66196d20bba6a19ff31
akathirvel <39790038+akathirvel@users.noreply.github.com>
2021-12-08 08:09:58 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ const app = express();
 const port = 8080; // default port to listen
 
 app.get( "/api", ( req: express.Request, res: express.Response ) => {
+    res.json({"hello": "inside api route"}) 
+} );
+app.get( "/", ( req: express.Request, res: express.Response ) => {
     res.json({"hello": "world"}) 
 } );
-
 app.listen( port, () => {
     console.log( `server started at http://localhost:${ port }` );
 } );
